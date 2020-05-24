a1260962995d2756a9a0e82ce72592dd70bd6fbc
TEspelien <tespelien@eastsideprep.org>
2020-05-23 21:08:42 -0700

    updated constructor calls

--- Aspen-lib/src/MathFunctions.java
+++ Aspen-lib/src/MathFunctions.java
@@ -1,11 +1,11 @@
 public class MathFunctions {
 
     public double mag(Vec2d v) {
-        return Mat.sqrt(v.x * v.x + v.y * v.y)
+        return Mat.sqrt(v.x * v.x + v.y * v.y);
     }
 
     public double mag(Vec3d v) {
-        return Mat.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+        return Mat.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
     }
 
 
@@ -21,29 +21,29 @@ public class MathFunctions {
         double x = v1.y * v2.z - v1.z * v2.y;
         double y = v1.z * v2.x - v1.x * v2.z;
         double z = v1.x * v2.y - v1.y * v2.x;
-        return new Vec3d(x, y, z);
+        return new cartesian(x, y, z);
     }
 
 
     public Vec2d scalarMult(double k, Vec2d v) {
-        return new Vec2d(v.x * k, v.y * k);
+        return new cartesian(v.x * k, v.y * k);
     }
 
     public Vec3d scalarMult(double k, Vec3d v) {
-        return new Vec2d(v.x * k, v.y * k, v * z * k);
+        return new cartesian(v.x * k, v.y * k, v * z * k);
     }
 
     public Vec2d matrixMult(double[][] m, Vec2d v) {
         double x = v.x * m[0][0] + v.y * m[0][1];
         double y = v.x * m[1][0] + v.y * m[1][1];
-        return new Vec2d(x, y);
+        return new cartesian(x, y);
     }
 
     public Vec2d matrixMult(double[][] m, Vec3d v) {
         double x = v.x * m[0][0] + v.y * m[0][1] + v.z * m[0][2];
         double y = v.x * m[1][0] + v.y * m[1][1] + v.z * m[1][2];
         double z = v.x * m[2][0] + v.y * m[2][1] + v.z * m[2][2];
-        return new Vec2d(x, y, z);
+        return new cartesian(x, y, z);
     }
 
 
@@ -53,6 +53,14 @@ public class MathFunctions {
 
     public Vec2d subtract(Vec2d v1, Vec2d v2) {
         return cartesian(v2.x - v1.x, v2.y - v1.y);
+    }
+
+    public Vec3d add(Vec3d v1, Vec3d v2) {
+        return cartesian(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+    }
+
+    public Vec2d subtract(Vec2d v1, Vec2d v2) {
+        return cartesian(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
     }
 
 
